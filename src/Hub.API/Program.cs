0c8ca3cf0af6d3e3ac8d23ebb0295e6f0c7f6538
narrrsss <narminalieva2328@gmail.com>
2023-06-19 02:17:30 +0400

    Dependency update

--- src/Hub.API/Program.cs
+++ src/Hub.API/Program.cs
@@ -1,3 +1,5 @@
+using Hub.API.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddPostgresDbContext(builder.Configuration);
+builder.Host.UseLoggingSetup(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ src/Hub.Domain/Entities/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Hub.Domain.Primitives;
+
+namespace Hub.Domain.Entities;
+
+public sealed class Team : Entity
+{
+    public Team(Guid teamId) : base(teamId)
+    {
+
+    }
+    public Team()
+    {
+
+    }
+    public string Name { get; set; }
+    public string Logo { get; set; }
+    public ICollection<TeamPlayer> TeamPlayers { get; set; }
+    //public Guid CountryId { get; set; }
+    //public Guid StadiumId { get; set; }
+
+}
+
+
+
+
