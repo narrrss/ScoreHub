5163c93a216b6d95a13d8cf8d37de17dc8c435d5
narrrsss <narminalieva2328@gmail.com>
2023-06-19 03:26:37 +0400

    Revert "Dependency update"
    This reverts commit 918739095a50fe36b0ef3c6230e861661188b9cc.

--- src/Hub.API/Program.cs
+++ src/Hub.API/Program.cs
@@ -1,5 +1,3 @@
-using Hub.API.Configuration;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,12 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddPostgresDbContext(builder.Configuration);
-builder.Host.UseLoggingSetup(builder.Configuration);
-
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
